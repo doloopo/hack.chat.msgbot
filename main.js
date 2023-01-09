967f3f09c0905ee23bdbf47f2c999a3128d5668b
doloopo <39017669+doloopo@users.noreply.github.com>
2023-01-09 23:49:53 +0800

    history when user joins

--- main.js
+++ main.js
@@ -2,22 +2,23 @@ var HackChat = require("hack-chat");
 var chat = new HackChat(); // Client group for multiple channels
 //chat.join("lobby", "TestUser");
 
-var namelist = ["testchannel001", "testchannel012"];
+var namelist = ["testchannel011"];
 var botName = "MsgBot"
 var sessions = [];
 
 var maxMsg = 100;
 var msgGiven = 20;
+var waitInterval = 1000; // should be set to 30000
 
 var msgList = [];
 // Init msgList
-for(var i = 0; i < namelist.length; i++) {
+for (var i = 0; i < namelist.length; i++) {
     msgList.push([]);
 }
 
-function sleep(ms){
-    var start = Date.now(), end = start+ms;
-    while(Date.now()<end);
+function sleep(ms) {
+    var start = Date.now(), end = start + ms;
+    while (Date.now() < end);
     return;
 }
 
@@ -26,7 +27,7 @@ function getLocalTime(n) {
 }
 
 function store(op, message) {
-    if(op.length < maxMsg){
+    if (op.length < maxMsg) {
         op.push(message);
     } else {
         op.splice(0, 1);
@@ -36,13 +37,13 @@ function store(op, message) {
 }
 
 // Join each channel
-var i = 0;  
-var id = setInterval(function(){
+var i = 0;
+var id = setInterval(function () {
     sessions.push(chat.join(namelist[i], botName));
     i++;
 
-    if(i>= namelist.length) clearInterval(id); 
-}, 1000);
+    if (i >= namelist.length) clearInterval(id);
+}, waitInterval);
 
 /*
 for(var i = 0; i<namelist.length; i++){
@@ -53,20 +54,40 @@ for(var i = 0; i<namelist.length; i++){
 //var testSession001 = chat.join("testChannel001", "TestUser");
 //chat.join("testChannel002", "TestUser");
 
-chat.on("onlineSet", function(session, users) {
+chat.on("onlineSet", function (session, users) {
     // Each event from a group contains a session argument as first argument
     console.log("Users online in ?" + session.channel + ": " + users.join(", "));
+    session.sendMessage("Hello everyone! MsgBot is online! ");
 });
 
-chat.on("chat", function(session, nick, text, timestamp) {
-    console.log(getLocalTime(timestamp)+"@"+nick + "@" + session.channel + ": " + text);
+chat.on("info", function (session, text, time) {
+    console.log("you're fucked. " + text);
+});
+chat.on("warn", function (session, text, time) {
+    console.log("you're fucked. " + text);
+});
 
-    var a = msgList[namelist.indexOf(session.channel)];
-    a = store(a, getLocalTime(timestamp)+"@"+nick+ ": " + text);
+chat.on("onlineAdd", function (session, nick, time) {
+    var messageListToBeSend = "[(del-" + nick + ") Hello, @" + nick + "! ]\n"
+        + "These are the most recent " + msgGiven + " messages (if possible): \n";
+    for(var i = 0; i < msgList[namelist.indexOf(session.channel)].length; i++){
+        messageListToBeSend += msgList[namelist.indexOf(session.channel)][i] + "\n";
+    }
 
-    console.log(a);
+    session.sendMessage(messageListToBeSend);
+})
+
+chat.on("chat", function (session, nick, text, timestamp) {
+    console.log(getLocalTime(timestamp) + "@" + nick + "@" + session.channel + ": " + text);
+
+    if (nick != botName) {
+        var a = msgList[namelist.indexOf(session.channel)];
+        a = store(a, getLocalTime(timestamp) + "@" + nick + ": " + text);
+
+        console.log(a);
+    }
     /*if (session.channel != "programming") {
         programmingSession.sendMessage("Quote from ?" + session.channel + ": " + text);
     }*/
-    
+
 });
