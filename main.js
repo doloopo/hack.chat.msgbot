42b051091ea329f46a1089b4c93accf3e81662aa
doloopo <39017669+doloopo@users.noreply.github.com>
2023-01-10 08:44:47 +0800

    listen to port 3410

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
+// Keep app going
+// Importing the express module
+const express = require('express');
+const app = express();
+
+// Initializing the data with the following string
+var data = "MsgBot is running. "
+
+// Sending the response for '/' path
+app.get('/' , (req,res)=>{
+
+   // Sending the data json text
+   res.send(data);
+})
+
+// Setting up the server at port 3000
+app.listen(3410 , ()=>{
+   console.log("server running");
+});
+
 var HackChat = require("hack-chat");
 var chat = new HackChat(); // Client group for multiple channels
 //chat.join("lobby", "TestUser");
